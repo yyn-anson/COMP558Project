bed41eef886119bf0bccee0090a5c5551888eaab
m3arg <98652128+m3arg@users.noreply.github.com>
2024-12-08 12:44:41 -0500

    50/50 Implemented

---
+++ new_drawSplitMATproperty.m
@@ -0,0 +1,83 @@
+function new_drawSplitMATproperty(vecLD, property, markerSize)
+    % my_drawSplitMATpropertyWithRectangles(vecLD, property, markerSize)
+    % splits the MAT property into top 50% and bottom 50%, visualizes them with
+    % rectangle outlines, as seen in paper
+    %
+    % Input:
+    %   vecLD: The vectorized line drawing
+    %   property: String indicating the MAT property ('mirror', 'parallelism', 'separation', 'taper', 'convexity')
+    %   markerSize: The size of the markers for plotting (default: 1)
+    %
+    % Output:
+    %   Saves three images: 'property'_top50.png, 'property'_bot50.png, 'property'_intact.png
+
+    if nargin < 3
+        markerSize = 1;
+    end
+
+
+    if ~isfield(vecLD, [property, '_allX'])
+        error(['Property ', property, ' has not been computed.']);
+    end
+
+    %same format as drawMATproperty
+    allX = vecLD.([property, '_allX']);
+    allY = vecLD.([property, '_allY']);
+    allScores = vecLD.([property, '_allScores']);
+
+    %splitting scores into top 50% and bottom 50%
+    medianScore = median(allScores);
+    topIdx = allScores > medianScore;    %top 50%
+    bottomIdx = allScores <= medianScore; %bottom 50%
+    topX = allX(topIdx);
+    topY = allY(topIdx);
+    bottomX = allX(bottomIdx);
+    bottomY = allY(bottomIdx);
+
+    imgWidth = vecLD.imsize(1);
+    imgHeight = vecLD.imsize(2);
+    rectPos = [1, 1, imgWidth - 1, imgHeight - 1];
+
+    %bottom 50% plot (black lines + blue rectangle)
+    figure('Visible', 'off');
+    scatter(bottomX, bottomY, markerSize, 'k', 'filled');
+    hold on;
+    rectangle('Position', rectPos, 'EdgeColor', 'b', 'LineWidth', 2);
+    hold off;
+    setAxisProperties(imgWidth, imgHeight);
+    saveas(gcf, [property, '_bot50.png']);
+    close(gcf);
+
+    %top 50% plot (black lines + red rectangle)
+    figure('Visible', 'off');
+    scatter(topX, topY, markerSize, 'k', 'filled');
+    hold on;
+    rectangle('Position', rectPos, 'EdgeColor', 'r', 'LineWidth', 2);
+    hold off;
+    setAxisProperties(imgWidth, imgHeight);
+    saveas(gcf, [property, '_top50.png']);
+    close(gcf);
+
+    %whole image plot (bottom 50 in blue, top 50 in red)
+    figure('Visible', 'off');
+    hold on;
+    scatter(bottomX, bottomY, markerSize, 'b', 'filled');
+    scatter(topX, topY, markerSize, 'r', 'filled');
+    hold off;
+    rectangle('Position', rectPos, 'EdgeColor', 'k', 'LineWidth', 2);
+    setAxisProperties(imgWidth, imgHeight);
+    legend({'Bottom 50%', 'Top 50%'}, 'Location', 'best');
+    saveas(gcf, [property, '_intact.png']);
+    close(gcf);
+
+
+    function setAxisProperties(imgWidth, imgHeight)
+        axis ij;
+        axis([1, imgWidth, 1, imgHeight]);
+        axis off;
+        daspect([1 1 1]);
+        box on;
+        set(gcf, 'color', 'w');
+        set(gca, 'Position', [0 0 1 1]);
+    end
+end
---
+++ testing_5050.m
@@ -0,0 +1,2 @@
+%replace param with what you want. must run testing_Properties first
+new_drawSplitMATproperty(vecLD, 'separation', 2);
